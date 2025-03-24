e811bb77fefb11add07e7637f05fb0fcea6b751d
pedroX4vier <pedroh.moura2024@gmail.com>
2025-03-24 16:01:58 -0300

    feat: criando a respositório de transação

---
+++ src/main/java/com/picpaysimplificado/domain/transaction/TransactionRepository.java
@@ -0,0 +1,6 @@
+package com.picpaysimplificado.domain.transaction;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long>{
+}
