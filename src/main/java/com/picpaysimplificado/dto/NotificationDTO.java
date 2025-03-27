0a21d8997a99dadc73f1a28845b09e9b003a760b
pedroX4vier <pedroh.moura2024@gmail.com>
2025-03-26 21:41:48 -0300

    feat: foi criado a classe Notification DTO

---
+++ src/main/java/com/picpaysimplificado/dto/NotificationDTO.java
@@ -0,0 +1,11 @@
+/* ANOT COD
+ * 
+ * 'record' - SERVE PARA REPRESENTAR DE FORMA CONCISA UM TIPO DE DADOS IMUTÁVEL QUE ARMAZENA UM CONJUNTO DE VALORES.
+ * 
+ * */
+
+package com.picpaysimplificado.dto;
+
+
+public record NotificationDTO(String email, String message) {
+}
