23c5e56a0113c8686397d2116f5c8c22eb107169
pedroX4vier <pedroh.moura2024@gmail.com>
2025-03-26 21:41:16 -0300

    feat: foi criado a pasta dto

---
+++ src/main/java/com/picpaysimplificado/dto/TransactionDTO.java
@@ -0,0 +1,12 @@
+/* ANOT COD
+ * 
+ * 'record' - SERVE PARA REPRESENTAR DE FORMA CONCISA UM TIPO DE DADOS IMUTÁVEL QUE ARMAZENA UM CONJUNTO DE VALORES.
+ * 
+ * */
+
+package com.picpaysimplificado.dto;
+
+import java.math.BigDecimal;
+
+public record TransactionDTO(BigDecimal value, Long senderId, Long receiverId) {
+}
