f65a0a68b91e896380e6d8c1eab78ead6d47482a
pedroX4vier <pedroh.moura2024@gmail.com>
2025-03-26 21:42:25 -0300

    feat: Foi criado e atualizado a TransactionService

---
+++ src/main/java/com/picpaysimplificado/services/TransactionService.java
@@ -0,0 +1,100 @@
+/* ANOT COD
+ * 'TransactionDTO' - RECEBE O PAYLOAD, REQUISIÇÃO DE POST(INFORMAÇÕES COLOCADAS NA REQUISIÇÃO)
+ *       - RECEBENDO O PAYLOAD PARA POSTERIORMENTE FAZER A TRANSAÇÕES
+ *      
+ *  O PROPRIO DESAFIO FORNECE OS O SERVIÇO TERCEIRO VIA HTTP PARA O TESTE 
+ *  
+ *  'RestTemplate' - CLASSE OFERECIDA PELO SPRING PARA INTERAÇÕES HTTP
+ *  
+ *  '(String) authorizationResponse.getBody().get("message");' REALIZAÇÃO DE UM CASTING
+ *       - FORÇA UM OBJETO DE UM TIPO PARA OUTRO TIPO, NESSE CASO DE UM OBJETO PARA STRING, PARA GUARDA-LO EM 'message'
+ *  
+ * */
+
+package com.picpaysimplificado.services;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.picpaysimplificado.domain.transaction.Transaction;
+import com.picpaysimplificado.domain.transaction.TransactionRepository;
+import com.picpaysimplificado.domain.user.User;
+import com.picpaysimplificado.dto.TransactionDTO;
+
+@Service
+public class TransactionService {
+	
+	//DECLARANDO DEPENDÊNCIAS
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	private TransactionRepository repository;
+	
+	//REST TEMPLATE PARA FAZER A REQUISIÇÃO HTTP 
+	@Autowired 
+	private RestTemplate restTemplate;
+	
+	
+
+	public void createTransaction(TransactionDTO transaction) throws Exception {
+		//BUSCA UM USUÁRIO PELO 'senderId' E O ARMAZENA NA VÁRIAVEL SENDER.
+		User sender = this.userService.findUserById(transaction.senderId()); //PELO ID DE QUEM ENVIOU(senderId)
+		
+		//BUSCA UM USUÁRIO PELO 'receiverId' E O ARMAZENA NA VÁRIAVEL REDEIVER.
+		User receiver = this.userService.findUserById(transaction.receiverId()); //PELO ID DE QUEM RECEBEU(receiverId)
+		
+		
+		//VALIDAÇÃO DO USUÁRIO
+		userService.validateTransaction(sender, transaction.value());
+		
+		//SALVANDO A ALTORIZAÇÃO DA TRANSAÇÃO EM UM BOOLEAN
+		boolean isAuthorized = this.authorizeTransaction(sender, transaction.value());
+		
+		
+		//VERIFICAÇÃO DE TRANSAÇÃO AUTORIZADA (SE NÃO ESTIVER AUTORIZADO LANÇA UMA EXCEÇÃO)
+		if(!isAuthorized) {
+			throw new Exception("Transação não autorizada! ");
+		}
+		
+		//CRIANDO NOVA TRANSAÇÃO NA TABELA
+		Transaction newTransaction = new Transaction();
+		newTransaction.setAmount(transaction.value());
+		newTransaction.setSender(sender);
+		newTransaction.setReceiver(receiver);
+		newTransaction.setTimestamp(LocalDateTime.now());
+		
+		
+		//ATUALIZAÇÃO DO SALDO DO USUÁRIO E SALVAR TRANSAÇÃO
+		/* 1° - SUBTRAI O VALOR DA CONTA DO REMETENTE
+		 * 2° - ADICIONA O VALOR DA TRANSAÇÃO AO SALDO DA CONTA DO DESTINATÁRIO 
+		 */
+		sender.setBalance(sender.getBalance().subtract(transaction.value())); 
+		receiver.setBalance(receiver.getBalance().add(transaction.value())); 
+		
+		//SALVANDO NO BANCO DE DADOS 
+		this.repository.save(newTransaction);
+		this.userService.saveUser(sender); 
+		this.userService.saveUser(receiver);
+		
+	}
+	
+	public boolean authorizeTransaction(User sender, BigDecimal value) {
+		
+		@SuppressWarnings("rawtypes")
+		ResponseEntity<Map> authorizationResponse = restTemplate.getForEntity("https://util.devi.tools/api/v2/authorize", Map.class);
+		
+		if(authorizationResponse.getStatusCode() == HttpStatus.OK) {
+			String message = (String) authorizationResponse.getBody().get("message");
+			//COMPARANDO A STRING 'Autorizado' COM O QUE VIER NA MENSAGEM
+			return "Autorizado".equalsIgnoreCase(message);
+		} else return false;
+	}
+}
