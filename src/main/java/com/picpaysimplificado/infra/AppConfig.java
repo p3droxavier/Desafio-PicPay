ffe2da0d52599b95d25032f740cd2454e274f74a
pedroX4vier <pedroh.moura2024@gmail.com>
2025-04-04 10:08:00 -0300

    Atualização da classe

--- src/main/java/com/picpaysimplificado/infra/AppConfig.java
+++ src/main/java/com/picpaysimplificado/infra/AppConfig.java
@@ -1,5 +1,21 @@
+/* ANOT COD
+ * 
+ * AQUI CRIA-SE O METODO QUE INDICA A INSTANCIA A SER INJETADA
+ * 
+ * O '@Bean' TEM O TRABALHO DE FAZER AS INJEÇÕES ADEQUADASNAS NAS CLASSES QUE DEMPENDEM DO 'RestTemplate'
+ * 
+ * */
+
 package com.picpaysimplificado.infra;
 
-public class AppConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
+@Configuration
+public class AppConfig {
+    @Bean
+    RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 }
