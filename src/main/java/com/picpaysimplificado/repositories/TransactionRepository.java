5e0464895cc2a1c74216490fda462a4ddd360fe8
pedroX4vier <pedroh.moura2024@gmail.com>
2025-04-23 08:19:16 -0300

    foi atualizado na pasta

---
+++ src/main/java/com/picpaysimplificado/repositories/TransactionRepository.java
@@ -0,0 +1,7 @@
+package com.picpaysimplificado.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.picpaysimplificado.domain.transaction.Transaction;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long>{
+}
